b5296300cc3f9e176b854e39ee1ddf3bda3ef2c3
fahred <turik20121998@gmail.com>
2019-11-18 22:51:18 +0300

    Updates

---
+++ precache-manifest.7004d56bcb9c598dca8388f116547224.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14798dcbe1d517c5c4137bfcd4b955aa",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "27da2937178700050c73",
+    "url": "/weather-react/static/css/main.dea93199.chunk.css"
+  },
+  {
+    "revision": "4f1e77cd52526cd828fe",
+    "url": "/weather-react/static/js/2.0636f0ee.chunk.js"
+  },
+  {
+    "revision": "27da2937178700050c73",
+    "url": "/weather-react/static/js/main.bc6ffaf7.chunk.js"
+  },
+  {
+    "revision": "3b6c34b7da06726cbcef",
+    "url": "/weather-react/static/js/runtime-main.eea62777.js"
+  },
+  {
+    "revision": "8e1d6ae8a751255f4198978be4edc6d7",
+    "url": "/weather-react/static/media/addButton.8e1d6ae8.svg"
+  },
+  {
+    "revision": "ff74bc442535cb57177e628b4951e77f",
+    "url": "/weather-react/static/media/backImg.ff74bc44.jpg"
+  },
+  {
+    "revision": "d5cb98e49040cf70348304d98cd0ff0a",
+    "url": "/weather-react/static/media/crossButton.d5cb98e4.svg"
+  },
+  {
+    "revision": "50e1ca377c99bf59d9dcfed1b343ca72",
+    "url": "/weather-react/static/media/icon.50e1ca37.svg"
+  }
+]);
